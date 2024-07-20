5dc16b7ac066accab7629c0606e248919d64d069
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-07-20 22:14:16 +0530

    addes separete function for random color

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ const button = document.querySelector(".btn");
 const heading = document.querySelector("h1");
 
 button.addEventListener("click", function () {
-  let randOne = Math.floor(Math.random() * 254 + 1);
-  let randTwo = Math.floor(Math.random() * 254 + 1);
-  let randThree = Math.floor(Math.random() * 254 + 1);
-
-  const newColor = `rgb(${randOne},${randTwo},${randThree})`;
+  const newColor = createRandomColor();
 
   heading.innerText = `Your new Color is : ${newColor}`;
   heading.style.color = "white";
   document.body.style.backgroundColor = newColor;
 });
+
+let createRandomColor = () => {
+  let randOne = Math.floor(Math.random() * 254 + 1);
+  let randTwo = Math.floor(Math.random() * 254 + 1);
+  let randThree = Math.floor(Math.random() * 254 + 1);
+  
+
+  return (newColor = `rgb(${randOne},${randTwo},${randThree})`);
+};
+
+let sample = (randOne, randTwo, randThree) => {
+  let name =  `${randOne}, ${randTwo}, ${randThree}`;
+  return name;
+};
